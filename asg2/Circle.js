8bd3d0396b5b507b5d09bba3385db843b70998f9
amgg <16143061+adrianmgg@users.noreply.github.com>
2022-10-19 00:24:50 -0700

    init asg2 from asg1

---
+++ asg2/Circle.js
@@ -0,0 +1,55 @@
+
+class VecBufThing {
+	constructor(bufSize, mode, color) {
+		this.buf = new Float32Array(bufSize);
+		this.mode = mode;
+		this.color = color;
+	}
+	
+	render() {
+		gl.uniform4fv(u_FragColor, this.color);
+		gl.bufferData(gl.ARRAY_BUFFER, this.buf, gl.DYNAMIC_DRAW);
+		gl.drawArrays(gl.TRIANGLE_FAN, 0, this.buf.length / 2);
+	}
+}
+
+class Circle extends VecBufThing {
+	constructor({x, y, size, steps, color, angle}) {
+		angle += Math.PI / 2; // have angle 0 point polys corner up
+
+		// special case for 3 steps, since we can just use a single triangle for that
+		if(steps === 3) {
+			super(6, gl.TRIANGLES, color);
+			this.buf = new Float32Array(6);
+			let theta0 = 0 / steps * Math.PI * 2 + angle;
+			let theta1 = 1 / steps * Math.PI * 2 + angle;
+			let theta2 = 2 / steps * Math.PI * 2 + angle;
+			this.buf[0] = x + Math.cos(theta0) * size / canvas.width;
+			this.buf[1] = y + Math.sin(theta0) * size / canvas.height;
+			this.buf[2] = x + Math.cos(theta1) * size / canvas.width;
+			this.buf[3] = y + Math.sin(theta1) * size / canvas.height;
+			this.buf[4] = x + Math.cos(theta2) * size / canvas.width;
+			this.buf[5] = y + Math.sin(theta2) * size / canvas.height;
+		}
+		else {
+			super((3 * 2) + ((steps - 1) * 1 * 2), gl.TRIANGLE_FAN, color);
+			for(let i = 0; i < steps; i++) {
+				let theta0 = i / steps * Math.PI * 2 + angle;
+				let theta1 = (i+1) / steps * Math.PI * 2 + angle;
+				if(i === 0) {
+					this.buf[0] = x;
+					this.buf[1] = y;
+					this.buf[2] = x + Math.cos(theta0) * size / canvas.width;
+					this.buf[3] = y + Math.sin(theta0) * size / canvas.height;
+					this.buf[4] = x + Math.cos(theta1) * size / canvas.width;
+					this.buf[5] = y + Math.sin(theta1) * size / canvas.height;
+				}
+				else {
+					const baseIdx = 6 + (i-1) * 2;	
+					this.buf[baseIdx + 0] = x + Math.cos(theta1) * size / canvas.width;
+					this.buf[baseIdx + 1] = y + Math.sin(theta1) * size / canvas.height;
+				}
+			}
+		}
+	}
+}
---
+++ asg2/asg1.js
@@ -0,0 +1,230 @@
+const PRESERVE_BUFFER = false;
+
+// Vertex shader program
+var VSHADER_SOURCE =`
+attribute vec4 a_Position;
+attribute float a_PointSize;
+void main() {
+	gl_Position = a_Position;
+	gl_PointSize = a_PointSize;
+}
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+precision mediump float;
+uniform vec4 u_FragColor;
+void main() {
+  gl_FragColor = u_FragColor;
+}
+`;
+
+/** @type {WebGLRenderingContext} */
+let gl;
+
+let canvas, a_Position, u_FragColor, a_PointSize;
+let points = [];
+
+function main() {
+	setupWebGL();
+	setupShaders();
+	setupBuffers();
+	initUI();
+	canvas.addEventListener('mousedown', onCanvasMouseDown);
+	canvas.addEventListener('mousemove', onCanvasMouseMove);
+	canvas.addEventListener('mouseout', onCanvasMouseOut);
+	canvas.addEventListener('mouseup', onCanvasMouseUp);
+	renderAll();
+}
+
+function setupWebGL() {
+	// Retrieve <canvas> element
+	canvas = document.getElementById('canvas');
+
+	// Get the rendering context for WebGL
+	// gl = getWebGLContext(canvas);
+	gl = WebGLUtils.setupWebGL(canvas, {preserveDrawingBuffer: PRESERVE_BUFFER});
+	if (!gl) {
+		console.log('Failed to get the rendering context for WebGL');
+		return;
+	}
+	
+	clearCanvas();
+}
+
+function setupShaders() {
+	// Initialize shaders
+	if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+		console.log('Failed to intialize shaders.');
+		return;
+	}
+
+	// // Get the storage location of a_Position
+	a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+	if (a_Position < 0) {
+		console.log('Failed to get the storage location of a_Position');
+		return;
+	}
+	
+	a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+	if(a_PointSize < 0) {
+		console.log('Failed to get the storage location of a_PointSize');
+		return;
+	}
+
+	// Get the storage location of u_FragColor
+	u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+	if (!u_FragColor) {
+		console.log('Failed to get the storage location of u_FragColor');
+		return;
+	}
+}
+
+function setupBuffers() {
+	const vertexBuffer = gl.createBuffer();
+	if(!vertexBuffer) {
+		console.log('failed to create vertex buffer');
+		return -1;
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.enableVertexAttribArray(a_Position);
+	gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+}
+
+function clearCanvas() {
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+// function clearAll() {
+// 	clearCanvas();
+// 	points = [];
+// }
+
+function renderAll() {
+	clearCanvas();
+
+	for(const point of points) {
+		point.render();
+	}
+}
+
+function pushPoint(point) {
+	points.push(point);
+	renderAll();
+}
+
+function popPoint() {
+	if(PRESERVE_BUFFER) {
+		throw 'unsupported';
+	} else {
+		points.pop();
+		renderAll();
+	}
+}
+
+function clearPoints() {
+	points.length = 0; // clear points array in-place
+	renderAll();
+}
+
+/**
+ * @param {MouseEvent} ev
+ * @returns {[number, number]}
+ */
+function mouseevent2canvascoords(ev) {
+	let x = ev.clientX;
+	let y = ev.clientY;
+	let rect = ev.target.getBoundingClientRect();
+	x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+	y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+	return [x, y];
+}
+
+let curStroke = null;
+
+function onCanvasMouseDown(ev) {
+	const [x, y] = mouseevent2canvascoords(ev);
+
+	if(paintProgramOptions.mode === 'stamp') {
+		lastDrawPoint = [x, y];
+		if(paintProgramOptions.stampOptions.mode === 'ngon') {
+			pushPoint(new Circle({
+				x,
+				y,
+				size: paintProgramOptions.stampOptions.size,
+				steps: paintProgramOptions.stampOptions.sides,
+				color: [...paintProgramOptions.stampOptions.color, paintProgramOptions.stampOptions.alpha],
+				angle: paintProgramOptions.stampOptions.angle,
+			}));
+		}
+	} else if(paintProgramOptions.mode === 'brush') {
+		// // TODO if curstroke not null
+		// curStroke = new BrushStroke();
+		// pushPoint(curStroke);
+	}
+}
+
+/** @type {[number, number] | null} */
+let lastDrawPoint = null;
+
+/**
+ * @param {MouseEvent} ev
+ */
+function onCanvasMouseMove(ev) {
+	const [x, y] = mouseevent2canvascoords(ev);
+	if(paintProgramOptions.mode === 'stamp') {
+		if(ev.buttons === 0) {
+			renderAll();
+			new Circle({
+				x,
+				y,
+				size: paintProgramOptions.stampOptions.size,
+				steps: paintProgramOptions.stampOptions.sides,
+				color: [...paintProgramOptions.stampOptions.color, paintProgramOptions.stampOptions.alpha],
+				angle: paintProgramOptions.stampOptions.angle,
+			}).render();
+		} else if(ev.buttons === 1) {
+			if(lastDrawPoint === null) {
+				lastDrawPoint = [x, y];
+			} else {
+				const dist = Math.sqrt(Math.pow(x - lastDrawPoint[0], 2) + Math.pow(y - lastDrawPoint[1], 2));
+				if(dist >= 1e-1) {
+					lastDrawPoint = [x, y];
+					pushPoint(new Circle({
+						x,
+						y,
+						size: paintProgramOptions.stampOptions.size,
+						steps: paintProgramOptions.stampOptions.sides,
+						color: [...paintProgramOptions.stampOptions.color, paintProgramOptions.stampOptions.alpha],
+						angle: paintProgramOptions.stampOptions.angle,
+					}));
+					renderAll();
+				}
+			}
+		}
+	} else if(paintProgramOptions.mode === 'brush') {
+		// if(curStroke !== null && ev.buttons === 1) {
+		// 	curStroke.points.push([x, y]);
+		// 	renderAll();
+		// }
+	}
+}
+
+/**
+ * @param {MouseEvent} ev
+ */
+function onCanvasMouseOut(ev) {
+	if(paintProgramOptions.mode === 'stamp') {
+		renderAll();
+		lastDrawPoint = null;
+	}
+}
+
+/**
+ * @param {MouseEvent} ev
+ */
+function onCanvasMouseUp(ev) {
+	lastDrawPoint = null;
+}
+
